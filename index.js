b13e70af01b5f2b561c2a7bb3597de5c1cc01092
Mary Alice Moore <46582241+mamooredesigns@users.noreply.github.com>
2025-01-27 15:44:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const skillsSection = document.querySelector("#skills-box");
 
 for (let i = 0; i < skills.length; i++) {
     let skillBubble = document.createElement('li');
+    console.log(skillBubble);
     skillBubble.classList.add("skill-item");
     skillsSection.appendChild(skillBubble);
     skillBubble.innerHTML = skills[i];
