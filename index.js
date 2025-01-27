7abdd14d592aab254e6e5eced112138777fc956a
Mary Alice Moore <46582241+mamooredesigns@users.noreply.github.com>
2025-01-27 15:08:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
+// footer section
 
+const foot = document.createElement('footer');
+foot.className = 'footer';
+document.body.append(foot);
+
+
+// Date and Year Information
+
+
+let today = new Date();
+let thisYear = today.getFullYear();
+
+
+//copyright section, which uses the Date/Year and appends to the footer
+
+
+const copyright = document.createElement('p');
+copyright.innerHTML = "© Mary Alice Moore " + thisYear;
+
+foot.appendChild(copyright);
+
+
+//skills section and list, function that creates a bulleted list
+
+
+const skills = ["Python", "JavaScript", "CLass Coordinating", "HTML", "CSS"];
+const skillsSection = document.querySelector("#skills-box");
+
+for (let i = 0; i < skills.length; i++) {
+    let skillBubble = document.createElement('ul');
+    skillBubble.classList.add("skill-item");
+    skillsSection.appendChild(skillBubble);
+    skillBubble.innerHTML = skills[i];
+};
+
+
+//getting info from GitHub via Fetch and sticking it in the elements
+
+
+fetch("https://api.github.com/users/mamooredesigns/repos")
+    .then((response) => {
+        if (response.ok) {
+            return response.text();
+        } else {
+        throw new Error("Whoops!  We've got an error!");
+        }
+    })
+    .then((data) => {
+        const repositories = JSON.parse(data);
+        //console.log(repositories);
+
+        const projectSection = document.getElementById("projects-box");
+        let projectList = projectSection.querySelector("ul");
+        projectSection.appendChild(projectList);
+
+        for(let repository of repositories) {
+            let project = document.createElement("li");
+            project.innerHTML = `<a class="proj-link" href="${repository.html_url}">${repository.name}</a>`;
+            projectList.appendChild(project);
+        }
+    })
+    .catch((error) => {
+        console.error("Try again later...", error.message)
+    });
+
+
+//message section
+
+
+let messageForm = document.querySelector("[name='haiku']");
+let messageSection = document.getElementById("message-section");
+let messageList = messageSection.querySelector("ul");
+messageSection.hidden = true;
